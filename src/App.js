aeac09f5274b61ab96084f50cd5ffcf248d0842f
Saurabh Lal <slal@plex.com>
2021-11-27 20:24:54 +0530

    reverted routes path

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
   return (
     <Switch>
       <Route path="/" exact component={Login} />
-      <ProtectedRoute path="/receipt-book-site/home" component={Home} />
-      <ProtectedRoute path="/receipt-book-site/account" component={Account} />
-      <ProtectedRoute path="/receipt-book-site/logout" component={Logout} />
+      <ProtectedRoute path="/home" component={Home} />
+      <ProtectedRoute path="/account" component={Account} />
+      <ProtectedRoute path="/logout" component={Logout} />
     </Switch>
   )
 }
