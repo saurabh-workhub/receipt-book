3fe443729db73ada1169b9afe0473d704dcd6cde
Saurabh Lal <slal@plex.com>
2021-11-27 21:16:10 +0530

    Test

--- src/auth/auth-provider.js
+++ src/auth/auth-provider.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const AuthProvider = ({ children }) => {
+  const history = useHistory();
   const domain = process.env.REACT_APP_APP_DOMAIN;
   const clientId = process.env.REACT_APP_APP_CLIENT_ID;
+
+  const onRedirectCallback = (appState) => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
+      onRedirectCallback={onRedirectCallback}
     >
       {children}
     </Auth0Provider>
