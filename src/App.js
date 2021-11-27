bf35398675f1dba2524cda7ea8560aefa8d773bb
Saurabh Lal <slal@plex.com>
2021-11-28 00:59:36 +0530

    Updated routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "./components/loading";
 import Home from "./components/home";
 import ProtectedRoute from "./auth/protected-route";
 import Account from "./components/account";
 import Logout from "./components/logout";
-import Login from "./components/login";
 
 function App() {
   const { isLoading } = useAuth0();
@@ -16,8 +15,8 @@ function App() {
 
   return (
     <Switch>
-      <Route path="/" exact component={Login} />
-      <ProtectedRoute path="/home" component={Home} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/home" component={Home} />
       <ProtectedRoute path="/account" component={Account} />
       <ProtectedRoute path="/logout" component={Logout} />
     </Switch>
