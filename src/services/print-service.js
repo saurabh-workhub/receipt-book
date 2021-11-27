53d3daf74124ea560fb8b2e222024774b9678ec5
Saurabh Lal <slal@plex.com>
2021-11-27 18:28:10 +0530

    refactored

--- src/services/print-service.js
+++ src/services/print-service.js
@@ -44,10 +44,8 @@ const PrintService = {
       var newLine = '\x0A'; //LF byte in hex notation
 
       var cmds = esc + "@"; //Initializes the printer (ESC @)
-      cmds += esc + '!' + '\x38'; //Emphasized + Double-height + Double-width mode selected (ESC ! (8 + 16 + 32)) 56 dec => 38 hex
       cmds += 'BEST DEAL STORES'; //text to print
       cmds += newLine + newLine;
-      cmds += esc + '!' + '\x00'; //Character font A selected (ESC ! 0)
       cmds += 'COOKIES                   5.00';
       cmds += newLine;
       cmds += 'MILK 65 Fl oz             3.78';
@@ -62,9 +60,7 @@ const PrintService = {
       cmds += newLine;
       cmds += 'CASH DUE                  0.78';
       cmds += newLine + newLine;
-      cmds += esc + '!' + '\x18'; //Emphasized + Double-height mode selected (ESC ! (16 + 8)) 24 dec => 18 hex
       cmds += '# ITEMS SOLD 2';
-      cmds += esc + '!' + '\x00'; //Character font A selected (ESC ! 0)
       cmds += newLine + newLine;
       cmds += '11/03/13  19:53:17';
 
